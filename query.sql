b6568166d574901c4b67816faf7ecf73046663f4
rrayatec <79470126+rrayatec@users.noreply.github.com>
2023-11-08 08:55:18 -0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `iot_2023` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `iot_2023`;
 USE `iot_2023`;
 
 
 DROP TABLE IF EXISTS `dht_sensor_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `dht_sensor_data` (
   `id` int NOT NULL AUTO_INCREMENT,
   `humidity` float DEFAULT NULL,
